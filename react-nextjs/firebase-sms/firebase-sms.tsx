1769a4bd24eeb25900de2a77345ead8c1d5a888b
crytonomics_runsystem <@duycnt>
2022-04-06 14:58:07 +0700

    feat: add functions send code sms firebase

---
+++ react-nextjs/firebase-sms/firebase-sms.tsx
@@ -0,0 +1,59 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import {
+  browserPopupRedirectResolver,
+  browserSessionPersistence,
+  ConfirmationResult,
+  initializeAuth,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+} from "firebase/auth";
+import { app, IFirebaseSmsContext } from ".";
+
+declare const window: Window &
+  typeof globalThis & {
+    recaptchaVerifier: RecaptchaVerifier;
+  };
+
+const FirebaseSmsDefault: IFirebaseSmsContext = {
+  phone: "",
+  onSendSms: () => {},
+  setphone: () => {},
+};
+
+export const FirebaseSmsContext =
+  createContext<IFirebaseSmsContext>(FirebaseSmsDefault);
+
+const FirebaseSmsProvider: React.FC = ({ children }) => {
+  const [phone, setphone] = useState<string>("");
+
+  const auth = initializeAuth(app, {
+    persistence: browserSessionPersistence,
+    popupRedirectResolver: browserPopupRedirectResolver,
+  });
+
+  // load firebase sms captcha
+  useEffect(() => {
+    window.recaptchaVerifier = new RecaptchaVerifier(
+      "sign-in-button",
+      {
+        size: "invisible",
+      },
+      auth
+    );
+  }, [auth]);
+
+  const onSendSms = async (): Promise<ConfirmationResult> =>
+    await signInWithPhoneNumber(auth, phone, window.recaptchaVerifier);
+
+  return (
+    <FirebaseSmsContext.Provider value={{ phone, onSendSms, setphone }}>
+      {children}
+    </FirebaseSmsContext.Provider>
+  );
+};
+
+const useFirebaseSms = () => {
+  return useContext(FirebaseSmsContext);
+};
+
+export { FirebaseSmsProvider, useFirebaseSms };
---
+++ react-nextjs/firebase-sms/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCFr2qVLR8wX4JBJVrqfFn-NGw1151MD_M",
+  authDomain: "phonelogin-7d955.firebaseapp.com",
+  projectId: "phonelogin-7d955",
+  storageBucket: "phonelogin-7d955.appspot.com",
+  messagingSenderId: "383926165251",
+  appId: "1:383926165251:web:575095b02985e93a545336",
+  measurementId: "G-Y1CKGP8B36",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
